a3befecf692576eb4fc3a21df7fbd1778aa7c67e
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-03-11 00:19:33 +0100

    Add status page

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  var url = 'https://status.app.dnt.no/api/v1/checks';
+
+  $.getJSON(url).done(status);
+
+  var $panel = $('#panel');
+  var $apps = $('#apps');
+  var $services = $('#services');
+
+  function status(data) {
+    data.checks = data.checks.map(function(check) {
+      check.class = check.status === 'up' ? 'operational' : 'major outage';
+      check.text = check.status === 'up' ? 'operativ' : 'ute av drift';
+      check.category = check.tags.reduce(function(cat, tag) {
+        return tag.name === 'service' ? 'service' : cat;
+      }, 'app');
+
+      return check;
+    });
+
+    var status = data.checks.reduce(function(status, check) {
+      return check.status !== 'up' ? 'major outage' : status;
+    }, 'operational');
+
+    $panel.attr('class', 'panel ' + status);
+    $panel.html(status === 'operational' ? 'Alle systemer er operative.' : 'Ett eller flere systemer ute av drift');
+
+    data.checks.forEach(function(item) {
+      var $here = item.category === 'service' ? $services : $apps;
+
+      var name = item.name;
+      var clas = item.class;
+      var text = item.text;
+
+      $here.append('<li>' + name + ' <span class="status ' + clas + '">' + text + '</span></li>') });
+  };
+});
