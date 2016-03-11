c47096f9e1831a0db4d5ad4d2455bfa1980c712d
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-03-11 14:10:53 +0100

    Minor rephrasing of words and definitions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-  var url = 'https://status.app.dnt.no/api/v1/checks';
-
-  $.getJSON(url).done(status);
+  $.getJSON('https://status.app.dnt.no/api/v1/checks').done(status);
 
   var $panel = $('#panel');
   var $apps = $('#apps');
@@ -10,7 +8,7 @@ $(document).ready(function() {
   function status(data) {
     data.checks = data.checks.map(function(check) {
       check.class = check.status === 'up' ? 'operational' : 'major outage';
-      check.text = check.status === 'up' ? 'operativ' : 'ute av drift';
+      check.text = check.status === 'up' ? 'operativ' : 'driftsavbrudd';
       check.category = check.tags.reduce(function(cat, tag) {
         return tag.name === 'service' ? 'service' : cat;
       }, 'app');
@@ -18,7 +16,7 @@ $(document).ready(function() {
       // check time since last outage
       if (check.status === 'up' && Date.now() - (check.lasterrortime * 1000) <= 86400000) {
         check.class = 'degraded performance';
-        check.text = 'ustabil';
+        check.text = 'ustabilt';
       }
 
       return check;
